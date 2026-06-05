5a46d0ed540fc3bfdd3cb3f117d6ff2465844934
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:57 -0500

    xml commenting

--- src/Soenneker.Twilio.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Twilio.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Twilio.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Twilio.OpenApiClient";
 }
